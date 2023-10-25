d51fad9aa9c83508c29f3798543e51b6d49a077d
iamgeorge <wangxiiq200@icloud.com>
2023-10-25 21:14:55 +0800

    savr

---
+++ ALevel_CS/test.py
@@ -0,0 +1,21 @@
+from array import *
+
+T = [[11, 12, 5, 2], [15, 6, 10], [10, 8, 12, 5], [12, 15, 8, 6]]
+
+
+# print(T)
+
+animals = ['cat', 'dog', 'rabbit']
+
+for a in animals:
+    print(a)
+
+for row in T:
+    # print(row)
+    for rol in row:
+        print(rol, end=" ")
+
+# for r in T:
+#     for c in r:
+#         print(c, end=" ")
+#     print()
